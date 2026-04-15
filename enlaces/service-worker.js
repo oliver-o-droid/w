5802c844c821cfd6d022fa9b4552c9b720c29246
Oliver-Lenovo <oliveroblanca@gmail.com>
2026-04-16 00:32:55 +0200

    app

--- enlaces/service-worker.js
+++ enlaces/service-worker.js
@@ -13,7 +13,36 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Cache abierto');
+        return cache.addAll(urlsToCache);const CACHE_NAME = 'enlaces-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  // Puedes añadir aquí tus archivos CSS y JS si los separas en el futuro
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
+];
+
+// Instala el Service Worker y cachea los archivos principales
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
         return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercepta las peticiones y sirve desde la caché si es posible
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
       })
   );
 });
